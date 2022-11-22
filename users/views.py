f2691e9cec50aff1fae108df2d3967fc5a361051
unknown <darnelldanny1997@gmail.com>
2022-11-21 16:48:12 -0800

    Created a registration view

--- users/views.py
+++ users/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from django.contrib import messages
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+def home(request):
+    return render(request, 'users/home.html')
+
+def register(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.isvalid():
+            form.save()
+            messages.success(request, f'Your Account has been created successfully. You can now log in using the credentials you have used!')
+            return redirect('login')
+
+    else:
+        form = UserCreationForm()
+
+    context = {'form': form}
+    return render(request, 'users/register.html', context)
